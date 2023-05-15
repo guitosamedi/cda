36c037a5bf2c779acd3eae55875249ef7fdc27ec
G33k0 <samedig@gmail.coom>
2023-05-15 13:03:11 +0200

    java

---
+++ 01-cours/05-Java/approche-imperative/src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,9 @@
+package fr.declaration.variable;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class DeclarationApp {
+    public static void main(String[] args) {
+        System.out.printf("Hello and welcome!");
+    }
+}
