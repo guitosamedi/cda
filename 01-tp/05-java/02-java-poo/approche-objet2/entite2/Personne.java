c3772678a33444dc285653ae239d031e9d7b8ce3
G33k0 <samedig@gmail.coom>
2023-05-22 10:32:04 +0200

    tp4

--- 01-tp/05-java/02-java-poo/approche-objet2/entite2/Personne.java
+++ 01-tp/05-java/02-java-poo/approche-objet2/entite2/Personne.java
@@ -8,12 +8,14 @@ public class Personne {
         public String nom;
         public AdressePostale adresse;
 
+        /*
         public Personne(String prenom, String nom) {
 
                 this.prenom = prenom;
                 this.nom = nom;
                 this.adresse = adresse;
-        }
+        }*/
+
         public Personne(String prenom, String nom, AdressePostale adresse) {
                 this.prenom = prenom;
                 this.nom = nom;
@@ -21,9 +23,9 @@ public class Personne {
         }
 
         public void afficherPersonne(){
-                Personne p2 = new Personne("Clint", "Eastwood", adresse);
-                String firstName = p2.prenom;
-                String lastName = p2.nom.toUpperCase();
+                Personne p = new Personne("Clint", "Eastwood", adresse);
+                String firstName = p.prenom;
+                String lastName = p.nom.toUpperCase();
                 System.out.println(firstName + " " + lastName);
         }
 
@@ -31,15 +33,14 @@ public class Personne {
                 this.nom=lastName;
         }
 
-        public void modifierPrenom(String firstName){
-                this.nom=firstName;
-                //afficherPersonne();
-                //System.out.println(lastName);
-        }
+        public void modifierPrenom(String firstName){this.nom=firstName;}
 
-        public void modifierAdresse(String AdressePostale ){
+        public void modifierAdresse(AdressePostale adresse){
                 this.adresse = adresse;
         }
 
+        public String getNom() {
+                return this.nom;
+        }
 
 }
---
+++ 01-tp/05-java/02-java-poo/approche-objet2/utils/TestMethodeStatic.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class TestMethodeStatic {
+    public static void main(String[] args) {
+        String chaine = "44";
+        int number =   Integer.parseInt(chaine);
+        int a= 10;
+        int b = 98;
+        int afficher= Integer.max(a, b);
+        System.out.println(afficher);
+    }
+}
