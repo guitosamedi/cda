96a85f9a3c0ac1ed4a9890265e504f8c4cb630f0
G33k0 <samedig@gmail.coom>
2023-05-23 17:59:23 +0200

    tp10 ex3

--- 01-tp/05-java/02-java-poo/approche-objet2/listes/FusionListe.java
+++ 01-tp/05-java/02-java-poo/approche-objet2/listes/FusionListe.java
@@ -6,8 +6,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-//https://www.techiedelight.com/fr/join-two-lists-java/
-
 public class FusionListe {
     public static void main(String[] args) {
         ArrayList<String> web = new ArrayList<>();
@@ -24,11 +22,10 @@ public class FusionListe {
         // List Avec Stream et Collector
         List<String> colors = Stream.concat(web.stream(), print.stream()).collect(Collectors.toList());
 
-        Iterator<String> liste = colors.iterator();
-        while(liste.hasNext()){
-            String Col = liste.next();
+        Iterator<String> list = colors.iterator();
+        while(list.hasNext()){
+            String Col = list.next();
             System.out.println(Col);
         }
-
     }
 }
