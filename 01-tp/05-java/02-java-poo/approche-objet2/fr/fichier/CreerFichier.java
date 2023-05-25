a0e42b38538b4d983d98bc85ad585367fcdd3a7d
G33k0 <samedig@gmail.coom>
2023-05-25 11:36:15 +0200

    tp15 pas fini

--- 01-tp/05-java/02-java-poo/approche-objet2/fr/fichier/CreerFichier.java
+++ 01-tp/05-java/02-java-poo/approche-objet2/fr/fichier/CreerFichier.java
@@ -2,24 +2,37 @@ package fr.fichier;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CreerFichier {
     public static void main(String[] args) throws IOException {
 
-        Path path = Paths.get("C:/recensement.csv");
-        List<String> AllLines = Files.readAllLines(path, StandardCharsets.UTF_8);
-        List<String> lines = new ArrayList<>();
-        lines.add(lines.toString());
-        String l = null;
-        for (int i = 0; i<= 100; i++){
-            l = lines.get(i);
-            System.out.println(l);
-         }
+        Path pathSource = Paths.get("D:/recensement.csv");
+        // --> Pas de Droits en écriture sur lecteur C: --> D:
+        Path pathDestination = Paths.get("D:/new-recensement.csv");
+        // On verifie si le fichier de destination existe
+        boolean exists = Files.exists(pathDestination);
 
+        System.out.println("Copie du fichier ===>");
+
+        if(!exists){
+            // Sinon oui on écrase
+            System.out.println("J'existe pas !");
+            //Files.move(pathSource, pathDestination, StandardCopyOption.REPLACE_EXISTING);
+            Files.copy(pathSource, pathDestination);
+        }
+
+        List<String> AllLines = Files.readAllLines(pathSource, StandardCharsets.UTF_8);
+        ArrayList<String> lines = new ArrayList<>(AllLines);
+
+        for (int i = 0; i< 101; i++){
+            //System.out.println(lines.get(i));
+            lines.add(lines.get(i));
+
+         }
+        // On utilise pas le même fichier donc pas d'APPEND nécessaire
+        Files.write(pathDestination, lines, StandardOpenOption.APPEND);
     }
 }
--- 01-tp/05-java/02-java-poo/approche-objet2/fr/fichier/LectureFichier.java
+++ 01-tp/05-java/02-java-poo/approche-objet2/fr/fichier/LectureFichier.java
@@ -5,10 +5,15 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LectureFichier {
     public static void main(String[] args) throws IOException {
+
+        ArrayList<Ville> villes =new ArrayList<>();
+
+
         Path path = Paths.get("C:/recensement.csv");
         boolean exists = Files.exists(path);
         boolean estFichier = Files.isRegularFile(path);
@@ -17,5 +22,10 @@ public class LectureFichier {
         System.out.println("existe: " + exists + "fichier: " + exists + "lecture: " + exists);
         System.out.println("**************************************");
         // System.out.println(lines);
+
+        String[] tokens = lines.split(";");
+        //villes.add(new Ville("Paris", 75000, "île de France", 5555666333 ));
+
+
     }
 }
