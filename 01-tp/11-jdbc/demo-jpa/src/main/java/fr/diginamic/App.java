21fbdd5dc0dacac1f6d12257d1e58bd8f005b45b
G33k0 <samedig@gmail.coom>
2023-06-15 15:30:21 +0200

    tp1 jpa mariadb fix ok

--- 01-tp/11-jdbc/demo-jpa/src/main/java/fr/diginamic/App.java
+++
@@ -1,8 +0,0 @@
-package fr.diginamic;
-
-public class App {
-    public static void main(String[] args) {
-          System.out.printf("Hello and welcome!");
-
-        }
-    }
---
+++ 01-tp/12-JPA/demo-jpa/src/main/java/fr/diginamic/App.java
@@ -0,0 +1,15 @@
+package fr.diginamic;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App {
+    public static void main(String[] args) {
+
+
+
+
+
+        }
+    }
